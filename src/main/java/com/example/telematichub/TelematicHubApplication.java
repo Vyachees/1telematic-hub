090c6d6e8b83008820264d4a1b7bde7f0a6911f5
Vyacheslav <kirillov.com@gmail.com>
2022-12-05 21:49:02 +0300

    beautify

--- src/main/java/com/example/telematichub/TelematicHubApplication.java
+++ src/main/java/com/example/telematichub/TelematicHubApplication.java
@@ -1,8 +1,8 @@
 package com.example.telematichub;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -11,17 +11,15 @@ import java.net.Socket;
 import static com.example.telematichub.Server.port;
 
 @SpringBootApplication
+@Slf4j
 public class TelematicHubApplication {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(TelematicHubApplication.class, args);
         ServerSocket srvSocket = null;
         try {
             try {
-                int i = 0; // Счётчик подключений
-                // Подключение сокета к localhost
+                int i = 0; // Count connects
+                // Connect to localhost
                 InetAddress ia;
                 ia = InetAddress.getByName("localhost");
                 srvSocket = new ServerSocket(port, 0, ia);
@@ -29,15 +27,13 @@ public class TelematicHubApplication {
                 System.out.println("Server started\n\n");
 
                 while(true) {
-                    // ожидание подключения
                     Socket socket = srvSocket.accept();
                     System.err.println("Client accepted");
-                    // Стартуем обработку клиента
-                    // в отдельном потоке
+                    // Start in separate thread
                     new Server().setSocket(i++, socket);
                 }
             } catch(Exception e) {
-                System.out.println("Exception : " + e);
+                log.info("Exception : " + e);
             }
         } finally {
             try {
@@ -49,41 +45,6 @@ public class TelematicHubApplication {
         }
         System.exit(0);
     }
-       /* public static final String topicExchangeName = "spring-boot-exchange";
-
-    static final String queueName = "spring-boot";
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
-    }
-*/
-
 }
 
 
